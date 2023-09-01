1693899dbf9e58b3e0a762783a48db4d69de40ee
Nathan Jones <njonessurvey@gmail.com>
2023-09-01 11:16:56 -0500

    add creation of navbar and body, buttons to navbar

--- src/elementCreation.js
+++ src/elementCreation.js
@@ -12,7 +12,7 @@ export function makeElement(element, parentElement, optionalClass, optionalID) {
 
 export function styleElementID (elementID, styleAttribute, styleValue){
     const elementToStyle = document.getElementById(elementID)
-    if(elementToStyle.style == "innerHTML"){
+    if(styleAttribute == "innerHTML"){
         elementToStyle.innerHTML = styleValue;
     } else {
         elementToStyle.style[styleAttribute] = styleValue;
@@ -26,3 +26,29 @@ export function styleElementClass (elementClass, styleAttribute, styleValue){
     elementToStyle.style[styleAttribute] = styleValue;
     console.log(`Styling ${elementToStyle} with ${styleAttribute} set to ${elementToStyle.style[styleAttribute]}`)
 }
+
+export function createNavAndBody (){
+    // Grid Container to host nav bar and body
+    makeElement("div", document.body, "", "page-container");
+    styleElementID("page-container", "display", "grid");
+    styleElementID("page-container", "grid-template-columns", "95vw");
+    styleElementID("page-container", "grid-template-rows", "11vh 88vh");
+    styleElementID("page-container", "padding-left", "2.5%");
+    styleElementID("page-container", "padding-top", "1%");
+    makeElement("div", document.getElementById("page-container"), "nav-bar-container", "home-nav-container");
+    makeElement("div", document.getElementById("page-container"), "body-container", "home-body-container");
+    // Make navbar a grid container for 4 buttons
+    styleElementID("home-nav-container", "display", "flex");
+    styleElementID("home-nav-container", "justify-content", "space-around");
+    const buttonText = ["Home", "Meats", "Veggies", "Desserts"];
+    for(let i = 0; i < 4; i++){
+        makeElement("button", document.getElementById("home-nav-container"), "nav-button", `nav-button-${i}`);
+        styleElementID(`nav-button-${i}`, "innerHTML", buttonText[i]);
+    }
+    // makeElement("p", document.getElementById("page-container"), "home-text", "home-text-intro");
+    // const introText = "Finally, an amazing restaurant for all of your <b>artisan</b> grilled cheese needs!";
+    // styleElementID("home-text-intro", "innerHTML", introText);
+}
+
+const styleVariable = document.body.style
+styleVariable.flex
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,26 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require("webpack");
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: {
+    app: './src/index.js',
+  },
   devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+    hot: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Hot Module Replacement',
+    }),
+  ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   module: {
     rules: [
